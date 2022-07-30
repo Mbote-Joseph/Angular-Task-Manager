9fca73ff4783a3e0ae68b0dd648be62e56aef0db
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-30 22:54:27 +0300

    Task service Addition

--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ITask } from './task';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
   url: string = 'http://localhost:3000/tasks';
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getTasks() {
+    return this.http.get(this.url);
+  }
+
+  getTask(id: number) {
+    return this.http.get(`${this.url}/${id}`);
+  }
+
+  createTask(task: ITask) {
+    return this.http.post(this.url, task);
+  }
+
+  updateTask(task: ITask) {
+    return this.http.put(`${this.url}/${task.id}`, task);
+  }
+
+  deleteTask(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+
+  completeTask(id: number) {
+    return this.http.put(`${this.url}/${id}/complete`, {});
+  }
+
+  incompleteTask(id: number) {
+    return this.http.put(`${this.url}/${id}/incomplete`, {});
+  }
 }
