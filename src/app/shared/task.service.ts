70c286bcb6557e27c1e47c5f150f16a1b3c8baa4
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-30 22:52:03 +0300

    HttpClient Module addition

--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
+  url: string = 'http://localhost:3000/tasks';
   constructor() {}
 }
