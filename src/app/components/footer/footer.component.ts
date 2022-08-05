bf10e7e17509687839e6bf53d4884186d50c2bac
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-05 20:41:09 +0300

    Incomplete Task Update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   date = new Date();
+  name: string = 'Mbote-Joseph';
 
   constructor() {}
 
